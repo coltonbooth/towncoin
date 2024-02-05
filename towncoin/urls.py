7f4a8d8ffc9f364c5fb934a1d70c55832da799e0
Colton Booth <95236600+coltonbooth@users.noreply.github.com>
2024-02-05 12:52:30 -0400

    Adding dashboard default

--- towncoin/urls.py
+++ towncoin/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import register_request, login_request, logout_request
+from wallet.views import WalletDashboardView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,5 @@ urlpatterns = [
     path('register/', register_request, name="register"),
     path('login/', login_request, name="login"),
     path('logout/', logout_request, name="logout"),
+    path('', WalletDashboardView.as_view(), name='dashboard'),
 ]
